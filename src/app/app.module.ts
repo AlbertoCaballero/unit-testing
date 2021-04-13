2d53823e410a1f290d1b03e3958a83ec3b3b2711
Alberto Sanchez Caballero <alberto.caballero@ibm.com>
2021-04-13 13:07:05 -0500

    Generated Share Button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShareButtonComponent } from './components/share-button/share-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShareButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/share-button/share-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-share-button',
+  templateUrl: './share-button.component.html',
+  styleUrls: ['./share-button.component.sass']
+})
+export class ShareButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
