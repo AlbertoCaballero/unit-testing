df10eb8dd61609246b3cd455fe7a1eb7eeba358f
Alberto Caballero <alberto.caballero@ibm.com>
2021-04-15 01:14:01 -0500

    Data Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ShareButtonComponent } from './components/share-button/share-button.component';
 import { ToggleButtonComponent } from './components/toggle-button/toggle-button.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ToggleButtonComponent } from './components/toggle-button/toggle-button.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  BASE_URL = 'https://jsonplaceholder.typicode.com';
+  TODOS_URL = `${this.BASE_URL}/todos`;
+
+  constructor(private http: HttpClient) {}
+
+  get(url: string): Observable<any> {
+    return this.http.get(url);
+  }
+
+  getTodo(todo: number): Observable<any> {
+    return this.get(`${this.TODOS_URL}/${todo}`);
+  }
+}
